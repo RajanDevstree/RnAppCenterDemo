b1601ccbdfef62658e5ffe7b065b00424bff3dc2
RajanDevstree <rajan.devstree@gmail.com>
2022-04-22 17:35:39 +0530

    react native androi push code set up with staging

--- App.js
+++ App.js
@@ -10,13 +10,16 @@ import {
   Image,
   Modal,
   ActivityIndicator,
+  Platform,
 } from 'react-native';
 import codePush from 'react-native-code-push';
 
 const codePushVersion = {
   'ios-test-variant-one': 'UVMEGI3ERLWZ6RCMH3xACllVYc-fUYMvl6r1q',
-  "ios-Staging": 'Yf5XtYTLNxIH0n54fWcJUdDI8j7_Ub0YQRD_E',
-  "ios-Production": '0rUrP1WJanW1mXkg9WMqZ-AOxbyPD2HEd1CGK',
+  'ios-Staging': 'Yf5XtYTLNxIH0n54fWcJUdDI8j7_Ub0YQRD_E',
+  'ios-Production': '0rUrP1WJanW1mXkg9WMqZ-AOxbyPD2HEd1CGK',
+  'android-Staging': 'rw_GyQFoqWsLLXbgHRuo6DBewlN0IVXudnoeW',
+  'android-Production': '_ciLkroQsmo3RqutxTkLkba29UZp9qTSsyQQ-',
 };
 
 let codePushOptions = {checkFrequency: codePush.CheckFrequency.MANUAL};
@@ -27,7 +30,7 @@ const App = () => {
   useEffect(() => {
     codePush.sync(
       {
-        deploymentKey: codePushVersion['ios-Staging'],
+        // deploymentKey: codePushVersion['android-Staging'],
         updateDialog: true,
         installMode: codePush.InstallMode.IMMEDIATE,
       },
@@ -127,63 +130,171 @@ const App = () => {
 
   return (
     <SafeAreaView
-      style={{flex: 1, backgroundColor: 'blue', marginHorizontal: 6}}>
+      style={{flex: 1, backgroundColor: '#a832a8', marginHorizontal: 0}}>
       <StatusBar barStyle={'dark-content'} />
       {!!progress ? showProgressView() : null}
 
-      <View style={{alignItems: 'center', marginTop: 30}}>
-        <Image
-          style={{width: 250, height: 60}}
-          source={{
-            uri: 'https://devblogs.microsoft.com/xamarin/wp-content/uploads/sites/44/2019/10/AppCenter_Logo.png',
-          }}
-        />
-      </View>
-      <View
-        style={{justifyContent: 'center', alignItems: 'center', marginTop: 30}}>
-        <Text style={{fontSize: 18, fontWeight: 'bold'}}>
-          App Stroe Version : 1.0 (1)
-        </Text>
-      </View>
-      <View
-        style={{justifyContent: 'center', alignItems: 'center', marginTop: 30}}>
-        <Text style={{fontSize: 18, fontWeight: 'bold'}}>
-          TestFlight Upload: Yes [code push connection no]
-        </Text>
-      </View>
-      <View
-        style={{justifyContent: 'center', alignItems: 'center', marginTop: 30}}>
-        <Text style={{fontSize: 18, fontWeight: 'bold'}}>
-          Build Schema: Debuge
-        </Text>
-      </View>
+      {Platform.OS == 'android' ? (
+        <>
+          <View style={{alignItems: 'center', marginTop: 30}}>
+            <Image
+              style={{width: 250, height: 60}}
+              source={{
+                uri: 'https://devblogs.microsoft.com/xamarin/wp-content/uploads/sites/44/2019/10/AppCenter_Logo.png',
+              }}
+            />
+          </View>
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 30,
+            }}>
+            <Text style={{fontSize: 18, fontWeight: 'bold'}}>
+              Android App Version : 1.0 (1)
+            </Text>
+          </View>
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 30,
+            }}>
+            <Text style={{fontSize: 18, fontWeight: 'bold'}}>
+              Playstore: NO [code push connection no]
+            </Text>
+          </View>
 
-      <View
-        style={{justifyContent: 'center', alignItems: 'center', marginTop: 30}}>
-        <Text style={{fontSize: 18, fontWeight: 'bold'}}>
-          CodePush Update: Yes
-        </Text>
-      </View>
-      <View
-        style={{justifyContent: 'center', alignItems: 'center', marginTop: 30}}>
-        <Text style={{fontSize: 18, fontWeight: 'bold'}}>
-          CodePush Version: 3.0 [codepush code app/xcode version: 1.0(1) not
-          change]
-        </Text>
-      </View>
-      <View
-        style={{justifyContent: 'center', alignItems: 'center', marginTop: 30}}>
-        <Text style={{fontSize: 18, fontWeight: 'bold', textAlign: 'center'}}>
-          code push staging
-        </Text>
-      </View>
-      <View
-        style={{justifyContent: 'center', alignItems: 'center', marginTop: 30}}>
-        <Text style={{fontSize: 18, fontWeight: 'bold', textAlign: 'center'}}>
-          Changes: 1 codepush upload and background color green and header image
-          change
-        </Text>
-      </View>
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 30,
+            }}>
+            <Text style={{fontSize: 18, fontWeight: 'bold'}}>
+              CodePush Update: Yes
+            </Text>
+          </View>
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 30,
+            }}>
+            <Text style={{fontSize: 18, fontWeight: 'bold'}}>
+              CodePush Version: 5.0
+            </Text>
+          </View>
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 30,
+            }}>
+            <Text
+              style={{fontSize: 18, fontWeight: 'bold', textAlign: 'center'}}>
+              code push staging
+            </Text>
+          </View>
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 30,
+            }}>
+            <Text
+              style={{fontSize: 18, fontWeight: 'bold', textAlign: 'center'}}>
+              Changes: 1 #a832a8 color set
+            </Text>
+          </View>
+        </>
+      ) : (
+        <>
+          <View style={{alignItems: 'center', marginTop: 30}}>
+            <Image
+              style={{width: 250, height: 60}}
+              source={{
+                uri: 'https://devblogs.microsoft.com/xamarin/wp-content/uploads/sites/44/2019/10/AppCenter_Logo.png',
+              }}
+            />
+          </View>
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 30,
+            }}>
+            <Text style={{fontSize: 18, fontWeight: 'bold'}}>
+              App Stroe Version : 1.0 (1)
+            </Text>
+          </View>
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 30,
+            }}>
+            <Text style={{fontSize: 18, fontWeight: 'bold'}}>
+              TestFlight Upload: Yes [code push connection no]
+            </Text>
+          </View>
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 30,
+            }}>
+            <Text style={{fontSize: 18, fontWeight: 'bold'}}>
+              Build Schema: Debuge
+            </Text>
+          </View>
+
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 30,
+            }}>
+            <Text style={{fontSize: 18, fontWeight: 'bold'}}>
+              CodePush Update: Yes
+            </Text>
+          </View>
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 30,
+            }}>
+            <Text style={{fontSize: 18, fontWeight: 'bold'}}>
+              CodePush Version: 3.0 [codepush code app/xcode version: 1.0(1) not
+              change]
+            </Text>
+          </View>
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 30,
+            }}>
+            <Text
+              style={{fontSize: 18, fontWeight: 'bold', textAlign: 'center'}}>
+              code push staging
+            </Text>
+          </View>
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 30,
+            }}>
+            <Text
+              style={{fontSize: 18, fontWeight: 'bold', textAlign: 'center'}}>
+              Changes: 1 codepush upload and background color green and header
+              image change
+            </Text>
+          </View>
+        </>
+      )}
     </SafeAreaView>
   );
 };
